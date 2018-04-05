bea839a2635dedca00fe576a18aa9fce121842fa
dcignarella <31837355+dcignarella@users.noreply.github.com>
2018-04-05 13:12:03 -0400

    Add files via upload

--- minimax_ai/aiTest.py
+++ minimax_ai/aiTest.py
@@ -2,19 +2,30 @@ from test import Board, Builder, prettyBoard, moveToString
 from convertion import moveToByte
 from ai import minimax
 import sys
+
+def moveExecute(move, board1, board2):
+    for i in range(len(board1.currentPlayer.legalMoves)):
+        if (board1.currentPlayer.legalMoves[i] == 0):
+            pass
+        else:
+            for j in range(len(x.currentPlayer.legalMoves[i])):
+                foundMove = board1.currentPlayer.legalMoves[i][j]
+                if (move == foundMove):
+                    board2 = board1.currentPlayer.legalMoves[i][j].execute()
+                    prettyBoard(board2.board)
+    return board2
+
+
 sys.setrecursionlimit(10000)
 a = 0
 
 y = Board(Builder()).createStandardBoard()
 x = Board(Builder()).createStandardBoard()
-#print(x.currentPlayer.getActivePieces())
 prettyBoard(x.board)
-for a in range(1):
+for a in range(10):
 #while (y.currentPlayer.isCheckmate()==False):
 #    a=a+1
-    #print("\n","\n")
     x = y
-    #print(x==y)
     depth = 3
     evaluate = minimax(x,depth)
     bestMove=evaluate.execute()
@@ -24,32 +35,4 @@ for a in range(1):
 #   moveToString(bestMove, y.currentPlayer))
     print("best move",bestMove)
     print("loop number",a)
-#    print("list size of legal moves", len(x.currentPlayer.legalMoves))
-    #not passing 9 because king is in check and isnt getting rid of it, not generating any attack moves
-    for i in range(len(x.currentPlayer.legalMoves)):
-        list = x.currentPlayer.legalMoves[i]
-        if (x.currentPlayer.legalMoves[i] == 0):
-            pass
-            # print(i, None)
-        else:
-#            print(x.currentPlayer.legalMoves[i])
-            for j in range(len(x.currentPlayer.legalMoves[i])):
-#                print(i,j)
-                move = x.currentPlayer.legalMoves[i][j]
-#                print("moves in list",x.currentPlayer.legalMoves[i][j],i,j)
-                if (bestMove==move):
-#                    print("index numbers",i,j)
-                    y=x.currentPlayer.legalMoves[i][j].execute()
-                    prettyBoard(y.board)
-#print(y.currentPlayer.getOpponent(), len(y.currentPlayer.getActivePieces()))
-#for i in range(len(y.currentPlayer.getOpponent().legalMoves)):
-#    if (y.currentPlayer.getOpponent().legalMoves[i] == 0):
-#        pass
-        # print(i, None)
-#    else:
-#        print(y.currentPlayer.getOpponent().legalMoves[i])
-#        for j in range(len(y.currentPlayer.legalMoves[i])):
-#            pass#print(y.currentPlayer.legalMoves[i][j])
-
-#print(y.board[21].getPiece())
-#print(y.board[12].getPiece())
+    y = moveExecute(bestMove, x, y)
